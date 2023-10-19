15c2fd1d2b9ebd8579514359a0be3ce65c8971b5
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-10-19 20:03:09 -0300

    🐛 fix(CategoryItem): added link to specific category

--- src/components/CategoryItem.tsx
+++ src/components/CategoryItem.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import { Category } from '@prisma/client';
 import { Badge } from './ui/Badge';
 import { categoryIcons } from '@/data/categoryIcons';
@@ -8,12 +10,14 @@ type CategoryItemProps = {
 
 export function CategoryItem({ category }: CategoryItemProps) {
   return (
-    <Badge
-      variant='outline'
-      className='py-3 flex justify-center items-center gap-2 rounded-lg'
-    >
-      {categoryIcons[category.slug as keyof typeof categoryIcons]}
-      <span className='font-bold text-xs'>{category.name}</span>
-    </Badge>
+    <Link href={`/category/${category.slug}`}>
+      <Badge
+        variant='outline'
+        className='py-3 flex justify-center items-center gap-2 rounded-lg'
+      >
+        {categoryIcons[category.slug as keyof typeof categoryIcons]}
+        <span className='font-bold text-xs'>{category.name}</span>
+      </Badge>
+    </Link>
   );
 }
