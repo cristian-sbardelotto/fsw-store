1c9c05384dd4dc8b9bc4278691778d60148f38c0
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-10-19 17:49:34 -0300

    ✨ feat(Home): added keyboards section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { Categories } from '@/components/Categories';
 import { ProductList } from '@/components/ProductList';
 import { prismaClient } from '@/lib/prisma';
+import { ImageBanner } from '@/components/ImageBanner';
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -13,15 +14,19 @@ export default async function Home() {
     },
   });
 
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: 'keyboards',
+      },
+    },
+  });
+
   return (
-    <main className='p-5'>
-      <Image
+    <main>
+      <ImageBanner
         src='/banner-home-01.png'
         alt='Até 55% de desconto esse mês!'
-        className='h-auto w-full px-5'
-        width={0}
-        height={0}
-        sizes='100vw'
       />
 
       <div className='mt-8 px-5'>
@@ -29,7 +34,20 @@ export default async function Home() {
       </div>
 
       <div className='mt-8'>
+        <p className='pl-5 mb-3 font-bold uppercase'>Ofertas</p>
         <ProductList products={deals} />
+      </div>
+
+      <div className='mt-8'>
+        <ImageBanner
+          src='/banner-home-02.png'
+          alt='Até 55% de desconto em mouses!'
+        />
+      </div>
+
+      <div className='mt-8'>
+        <p className='pl-5 mb-3 font-bold uppercase'>Teclados</p>
+        <ProductList products={keyboards} />
       </div>
     </main>
   );
