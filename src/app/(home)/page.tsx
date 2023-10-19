cece3475e38be6bfacb731c39eb3424ba7249789
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-10-19 17:58:23 -0300

    ✨ feat(Home): added mouses section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -22,32 +22,48 @@ export default async function Home() {
     },
   });
 
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: 'mouses',
+      },
+    },
+  });
+
   return (
-    <main>
+    <main className='space-y-8'>
       <ImageBanner
         src='/banner-home-01.png'
         alt='Até 55% de desconto esse mês!'
       />
 
-      <div className='mt-8 px-5'>
+      <div className='px-5'>
         <Categories />
       </div>
 
-      <div className='mt-8'>
+      <div>
         <p className='pl-5 mb-3 font-bold uppercase'>Ofertas</p>
         <ProductList products={deals} />
       </div>
 
-      <div className='mt-8'>
-        <ImageBanner
-          src='/banner-home-02.png'
-          alt='Até 55% de desconto em mouses!'
-        />
-      </div>
+      <ImageBanner
+        src='/banner-home-02.png'
+        alt='Até 55% de desconto em mouses!'
+      />
 
-      <div className='mt-8'>
+      <div>
         <p className='pl-5 mb-3 font-bold uppercase'>Teclados</p>
         <ProductList products={keyboards} />
+      </div>
+
+      <ImageBanner
+        src='/banner-home-03.png'
+        alt='Até 20% de desconto em fones!'
+      />
+
+      <div>
+        <p className='pl-5 mb-3 font-bold uppercase'>Mouses</p>
+        <ProductList products={mouses} />
       </div>
     </main>
   );
