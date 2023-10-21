281ba7e55905a1eeefdb84f1e7bcfc442edb4d35
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-10-21 16:34:26 -0300

    🔥 removed contexts folder

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,19 +1,48 @@
 'use client';
 
-import { CartContext } from '@/contexts/cart';
+import { createContext, useContext, useState } from 'react';
+
+import { Product } from '@prisma/client';
+
+type CartProduct = Product & {
+  quantity: number;
+};
+
+type CartContextProps = {
+  products: CartProduct[];
+  cartBasePrice: number;
+  cartTotalPrice: number;
+  cartTotalDiscount: number;
+  addProduct(product: CartProduct): void;
+};
+
+export const CartContext = createContext<CartContextProps>({
+  products: [],
+  cartBasePrice: 0,
+  cartTotalDiscount: 0,
+  cartTotalPrice: 0,
+  addProduct: () => {},
+});
 
 type CartContextProviderProps = {
   children: React.ReactNode;
 };
 
 export function CartContextProvider({ children }: CartContextProviderProps) {
+  const [products, setProducts] = useState<CartProduct[]>([]);
+
+  function addProduct(product: CartProduct) {
+    setProducts(previous => [...previous, product]);
+  }
+
   return (
     <CartContext.Provider
       value={{
-        products: [],
+        products,
         cartBasePrice: 0,
         cartTotalDiscount: 0,
         cartTotalPrice: 0,
+        addProduct,
       }}
     >
       {children}
