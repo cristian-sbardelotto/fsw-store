38e2cc2055d22a7eb729707333c4bc3d5f192ee4
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-10-21 11:22:50 -0300

    ✨ feat(ProductDetails): added relatedProducts section

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { ProductImages } from '@/components/ProductImages';
 import { ProductInfo } from '@/components/ProductInfo';
+import { ProductList } from '@/components/ProductList';
 import { computeProductTotalPrice } from '@/helpers/product';
 import { prismaClient } from '@/lib/prisma';
 import { SlugRouteParams } from '@/types/params';
@@ -9,18 +10,33 @@ export default async function ProductDetailsPage({ params }: SlugRouteParams) {
     where: {
       slug: params.slug,
     },
+    include: {
+      category: {
+        include: {
+          products: {
+            where: {
+              slug: {
+                not: params.slug,
+              },
+            },
+          },
+        },
+      },
+    },
   });
 
   if (!product) return null;
 
   return (
-    <div className='space-y-8'>
+    <div className='space-y-8 pb-8'>
       <ProductImages
         name={product.name}
         imageUrls={product.imageUrls}
       />
 
       <ProductInfo product={computeProductTotalPrice(product)} />
+
+      <ProductList products={product.category.products} />
     </div>
   );
 }
